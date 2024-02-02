b2387ddb4f444dee2ad9c1e27cd4721a78fa9ae9
Shashikant Rupin <shashikantrupin123@gmail.com>
2024-02-03 01:14:55 +0530

    json-server

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import AllRoutes from "./AllRoutes/AllRoutes";
+import "./App.css";
+// import { SingleProduct1 } from "./harshal/SingleProduct1";
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+       {/* < SingleProduct1/>      */}
+    </div>
+  );
+}
+
+export default App;
